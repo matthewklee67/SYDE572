1353371908f6e993863197eddeb954312e01e791
Matthew Lee <55307050+matthewklee67@users.noreply.github.com>
2023-01-26 13:43:06 -0500

    Added case 1 plots

--- Lab1.m
+++ Lab1.m
@@ -1,22 +1,40 @@
+close all
+
 % Case 1: Class 1
+nA = 200;
+classA = randn(nA,2);
+muA = [5, 10]';
+SA = [8, 0; 0, 4];
+classA = (classA*sqrtm(SA))' + muA;
+% Compare against MATLAB built in function
+testA = mvnrnd(muA, SA, nA);
 
-classA = randn([200 2])';
-muA = [5 10]';
-SA = [8 0; 0 4];
-classA1 = classA.*SA([1 2])';
-classA2 = classA.*SA([3 4])';
-classA = classA1 + classA2;
-classA = classA + muA;
-
-classB = randn([200 2])';
+nB = 200;
+classB = randn(nB, 2);
 muB = [10 15]';
 SB = [8 0; 0 4];
-classB1 = classB.*SB([1 2])';
-classB2 = classB.*SB([3 4])';
-classB = classB1 + classB2;
-classB = classB + muB;
+classB = (classB*sqrtm(SB))' + muB;
+% Compare against MATLAB built in function
+testB = mvnrnd(muB, SB, nB);
 
 figure
 scatter(classA(1,:),classA(2,:))
 hold on
+plot_ellipse(5,10,0,sqrt(8),sqrt(4),'k')
+hold on
 scatter(classB(1,:),classB(2,:))
+plot_ellipse(10,15,0,sqrt(8),sqrt(4),'k')
+legend('class A', 'unit SD equiprobability contour','class B')
+title('Manually Generated Gaussian Clusters')
+
+figure
+scatter(testA(:,1),testA(:,2))
+hold on
+plot_ellipse(5,10,0,sqrt(8),sqrt(4),'k')
+hold on
+scatter(testB(:,1),testB(:,2))
+hold on
+plot_ellipse(10,15,0,sqrt(8),sqrt(4),'k')
+legend('class A', 'unit SD equiprobability contour','class B')
+title('MATLAB Generated Gaussian Clusters')
+
