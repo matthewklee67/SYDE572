8fc00873e384ef4c74c5e79b13aa784873c1bb98
Matthew Lee <55307050+matthewklee67@users.noreply.github.com>
2023-04-01 16:05:05 -0400

    added part 3

--- lab2.m
+++ lab2.m
@@ -2,7 +2,7 @@ clear
 close all
 clc
 
-%% Model Estimation 1-D case:
+%% Part 2
 
 % Load given data for 1-D case
 data_case_1D = load('lab2_1.mat');
@@ -93,6 +93,90 @@ title('Parzen Estimation for Dataset B');
 legend('$p(x)$', '$\hat{p}(x)$ (Parzen, $\sigma=0.1$)',...
     '$\hat{p}(x)$ (Parzen, $\sigma=0.4$)', 'Interpreter', 'latex');
 
+%% Part 3
+% Load data
+data2_2 = load('lab2_2.mat');
+
+% Set necessary parameters
+grid_step = 1;
+parzen_sigma = sqrt(400);
+marksize = 25;
+colours = uint8([
+    255 175 175;  % light red
+    175 255 175;  % light green
+    175 175 255   % light blue
+]);
+
+% Make feature space grid
+x1_vals = [data2_2.al(:,1), data2_2.bl(:,1), data2_2.cl(:,1)];
+x2_vals = [data2_2.al(:,2), data2_2.bl(:,2), data2_2.cl(:,2)];
+[x1, x2] = make_grid(x1_vals, x2_vals, grid_step);
+
+% Parametric estimation
+[mu_a, S_a] = gauss_parameters(data2_2.al);
+[mu_b, S_b] = gauss_parameters(data2_2.bl);
+[mu_c, S_c] = gauss_parameters(data2_2.cl);
+
+pa_gauss2d = gauss_2d_likelihoods(x1, x2, mu_a, S_a);
+pb_gauss2d = gauss_2d_likelihoods(x1, x2, mu_b, S_b);
+pc_gauss2d = gauss_2d_likelihoods(x1, x2, mu_c, S_c);
+
+% Non-Parametric estimation
+% Declare Gaussian parzen window
+gfunc = @(x) exp(-0.5*(x(:,:,1).^2 + x(:,:,2).^2))/(2*pi);
+pdf_a = parzen_pdf(data2_2.al, gfunc, parzen_sigma);
+pdf_b = parzen_pdf(data2_2.bl, gfunc, parzen_sigma);
+pdf_c = parzen_pdf(data2_2.cl, gfunc, parzen_sigma);
+pa_parzen = parzen_2d_likelihoods(x1, x2, pdf_a);
+pb_parzen = parzen_2d_likelihoods(x1, x2, pdf_b);
+pc_parzen = parzen_2d_likelihoods(x1, x2, pdf_c);
+
+% Find maximum probability to obtain sample classes for parametric estimation based on ML
+p_g = pa_gauss2d;
+p_g(:, :, 2) = pb_gauss2d;
+p_g(:, :, 3) = pc_gauss2d;
+[~, idx_g] = max(p_g, [], 3);
+
+% Find maximum probability to obtain sample classes non-parametric estimation based on ML
+p_p = pa_parzen;
+p_p(:, :, 2) = pb_parzen;
+p_p(:, :, 3) = pc_parzen;
+[~, idx_p] = max(p_p, [], 3);
+
+% Plot samples and parametric estimation ML boundaries
+figure;
+hold on;
+contourf(x1, x2, idx_g, [1 2 3], 'k');
+colormap(colours);
+scatter(data2_2.al(:, 1), data2_2.al(:, 2), marksize,...
+    'MarkerEdgeColor','k', 'MarkerFaceColor', 'r');
+scatter(data2_2.bl(:, 1), data2_2.bl(:, 2), marksize,...
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'g');
+scatter(data2_2.cl(:, 1), data2_2.cl(:, 2), marksize,...
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'b');
+legend('Gaussian ML Boundaries', 'Cluster a', 'Cluster b', 'Cluster c');
+title('Clustered Data vs Gaussian ML Boundaries')
+xlabel('x1')
+ylabel('x2')
+hold off;
+
+% Plot samples and non-parametric estimation ML boundaries
+figure;
+hold on;
+contourf(x1, x2, idx_p, [1 2 3], 'k');
+colormap(colours);
+scatter(data2_2.al(:, 1), data2_2.al(:, 2), marksize,...
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'r');
+scatter(data2_2.bl(:, 1), data2_2.bl(:, 2), marksize,...
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'g');
+scatter(data2_2.cl(:, 1), data2_2.cl(:, 2), marksize,...
+    'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'b');
+legend('Parzen ML Boundaries', 'Cluster a', 'Cluster b', 'Cluster c');
+title('Clustered Data vs Parzen ML Boundaries')
+xlabel('x1')
+ylabel('x2')
+hold off;
+
 
 %% Functions used in this lab
 
@@ -167,4 +251,51 @@ end
 
 function plot_parzen_case_1D(input, density_parzen, line_format)
     plot(input, density_parzen, line_format, 'Linewidth', 2);
+end
+
+function likelihoods = gauss_2d_likelihoods(x1, x2, mu, S)
+    likelihoods = mvnpdf([x1(:) x2(:)], mu, S);
+    likelihoods = reshape(likelihoods, size(x1));
+end
+
+function likelihoods = parzen_2d_likelihoods(x1, x2, pdf)
+    likelihoods = pdf([x1(:) x2(:)]);
+    likelihoods = reshape(likelihoods, size(x1));
+end
+
+function pdf = parzen_pdf(x, phi, h)
+    d = size(x, 2);
+    % Calculate using Gaussian window, normalize using h and calculate mean
+    % to obtain parzen estimated PDF
+    pdf = @(x0) mean(phi(all_differences(x0, x)/h)/h^d);
+end
+
+function [mu, S] = gauss_parameters(x)
+    mu = mean(x);
+    S = (x-mu)'*(x-mu)/size(x,1);
+end
+
+function [x1, x2] = make_grid(x1vals, x2vals, step)
+    % Get original range of values
+    x1min0 = min(x1vals(:));
+    x1max0 = max(x1vals(:));
+    x2min0 = min(x2vals(:));
+    x2max0 = max(x2vals(:));
+
+    % Add 20% to width and height
+    dx1 = x1max0 - x1min0;
+    dx1 = dx1*1.2;
+    x1min = 0.5*(x1max0 + x1min0 - dx1);
+    x1max = 0.5*(x1max0 + x1min0 + dx1);
+    dx2 = x2max0 - x2min0;
+    dx2 = dx2*1.2;
+    x2min = 0.5*(x2max0 + x2min0 - dx2);
+    x2max = 0.5*(x2max0 + x2min0 + dx2);
+
+    % Generate gridpoints
+    nx1 = ceil((x1max - x1min)/step);
+    nx2 = ceil((x2max - x2min)/step);
+    x = linspace(x1min, x1max, nx1);
+    y = linspace(x2min, x2max, nx2);
+    [x1, x2] = meshgrid(x, y);
 end
